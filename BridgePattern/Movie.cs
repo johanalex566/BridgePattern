a6a41777830ee727a656769df0ff640ae80edcf1
johanalex566 <johanalex566@gmail.com>
2022-10-10 21:10:45 -0500

    Apply the bridge pattern

--- BridgePattern/Movie.cs
+++ BridgePattern/Movie.cs
@@ -4,109 +4,68 @@ namespace BridgePattern
 {
     public abstract class MovieLicense
     {
+        private readonly Discount _discount;
+
         public string Movie { get; }
         public DateTime PurchaseTime { get; }
 
-        protected MovieLicense(string movie, DateTime purchaseTime)
+        protected MovieLicense(string movie, DateTime purchaseTime, Discount discount)
         {
             Movie = movie;
             PurchaseTime = purchaseTime;
+            _discount = discount;
         }
 
-        public abstract decimal GetPrice();
+        public decimal GetPrice()
+        {
+            int discount = _discount.GetDiscount();
+            decimal multiplier = 1 - discount / 100m;
+            return GetPriceCore() * multiplier;
+        }
+
+        protected abstract decimal GetPriceCore();
         public abstract DateTime? GetExpirationDate();
+    }
+
+    public abstract class Discount
+    {
+        public abstract int GetDiscount();
+    }
+
+    public class NoDiscount : Discount
+    {
+        public override int GetDiscount() => 0;
+    }
+
+    public class MilitaryDiscount : Discount
+    {
+        public override int GetDiscount() => 10;
+    }
+
+    public class SeniorDiscount : Discount
+    {
+        public override int GetDiscount() => 20;
     }
 
     public class TwoDaysLicense : MovieLicense
     {
-        public TwoDaysLicense(string movie, DateTime purchaseTime)
-            : base(movie, purchaseTime)
+        public TwoDaysLicense(string movie, DateTime purchaseTime, Discount discount)
+            : base(movie, purchaseTime, discount)
         {
         }
 
-        public override decimal GetPrice()
-        {
-            return 4;
-        }
-
-        public override DateTime? GetExpirationDate()
-        {
-            return PurchaseTime.AddDays(2);
-        }
+        protected override decimal GetPriceCore() => 4;
+        public override DateTime? GetExpirationDate() => PurchaseTime.AddDays(2);
     }
-
-    public class MilitaryTwoDaysLicense : TwoDaysLicense
-    {
-        public MilitaryTwoDaysLicense(string movie, DateTime purchaseTime)
-            : base(movie, purchaseTime)
-        {
-
-        }
-
-        public override decimal GetPrice()
-        {
-            return base.GetPrice() * 0.9m;
-        }
-    }
-
-    public class SeniorTwoDaysLicense : TwoDaysLicense
-    {
-        public SeniorTwoDaysLicense(string movie, DateTime purchaseTime)
-            : base(movie, purchaseTime)
-        {
-
-        }
-
-        public override decimal GetPrice()
-        {
-            return base.GetPrice() * 0.8m;
-        }
-    }
-
 
     public class LifeLongLicense : MovieLicense
     {
-        public LifeLongLicense(string movie, DateTime purchaseTime)
-            : base(movie, purchaseTime)
+        public LifeLongLicense(string movie, DateTime purchaseTime, Discount discount)
+            : base(movie, purchaseTime, discount)
         {
         }
 
-        public override decimal GetPrice()
-        {
-            return 8;
-        }
-
-        public override DateTime? GetExpirationDate()
-        {
-            return null;
-        }
-    }
-
-    public class MilitaryLifeLongLicense : LifeLongLicense
-    {
-        public MilitaryLifeLongLicense(string movie, DateTime purchaseTime)
-            : base(movie, purchaseTime)
-        {
-
-        }
-
-        public override decimal GetPrice()
-        {
-            return base.GetPrice() * 0.9m;
-        }
-    }
-
-    public class SeniorLifeLongLicense : LifeLongLicense
-    {
-        public SeniorLifeLongLicense(string movie, DateTime purchaseTime)
-            : base(movie, purchaseTime)
-        {
-
-        }
-
-        public override decimal GetPrice()
-        {
-            return base.GetPrice() * 0.8m;
-        }
+        protected override decimal GetPriceCore() => 8;
+        public override DateTime? GetExpirationDate() => null;
     }
 }
